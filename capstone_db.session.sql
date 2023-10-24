6abe24beb68afda628ef0e5ef68f3c796a36318b
Jordan Chua Kai Jie <jordanvckj@gmail.com>
2023-10-24 11:02:15 +0800

    commented the sql commands

--- capstone_db.session.sql
+++ capstone_db.session.sql
@@ -1,11 +1,11 @@
-INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
-INSERT INTO roles (name) VALUES ('ROLE_TELLER');
-INSERT INTO users (username, first_name, last_name, password, enabled, role_id)
-VALUES (
-        'admin',
-        'Admin',
-        'Last Name',
-        '$2a$10$qLawnDP..8UX.KvfKZ9jBe1MZt1KR6bob/rs.111v8FJcPGYQ/qE.',
-        TRUE,
-        1
-    );
+-- INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
+-- INSERT INTO roles (name) VALUES ('ROLE_TELLER');
+-- INSERT INTO users (username, first_name, last_name, password, enabled, role_id)
+-- VALUES (
+--         'admin',
+--         'Admin',
+--         'Last Name',
+--         '$2a$10$qLawnDP..8UX.KvfKZ9jBe1MZt1KR6bob/rs.111v8FJcPGYQ/qE.',
+--         TRUE,
+--         1
+--     );
