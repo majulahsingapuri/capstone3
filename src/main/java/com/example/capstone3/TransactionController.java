2b7b9d3a3c8a0f1832b72b3ecabf3e8fde4a291a
cori-hwf <huangweifeng.cori@gmail.com>
2023-10-25 15:09:09 +0800

    validate debit is less than balance

--- src/main/java/com/example/capstone3/TransactionController.java
+++ src/main/java/com/example/capstone3/TransactionController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.capstone3.Transaction.TransactionType;
 
@@ -20,15 +21,24 @@ public class TransactionController {
 
     @Autowired AccountRepository accountRepository;
     @RequestMapping("/create")
-    public String showCreateTransaction(@RequestParam("accountId") Long accountId, Model model){
+    public String showCreateTransaction(@RequestParam("accountId") Long accountId,@RequestParam("balance") Double balance, Model model){
         model.addAttribute("accountId", accountId);
+        model.addAttribute("balance", balance);
         return "createtransaction";
     }
 
     @PostMapping("/create")
     public String createTransaction(@RequestParam("accountId") Long accountId,
                                 @RequestParam("transactionType") TransactionType transactionType,
-                                @RequestParam("amount") double amount) {
+                                @RequestParam("amount") double amount,
+                                @RequestParam("balance") Double balance,
+                                RedirectAttributes redirectAttributes) {
+
+        if (transactionType.equals(TransactionType.Debit) && amount > balance){
+            redirectAttributes.addFlashAttribute("error", "Insufficient balance. Please enter a smaller amount.");
+          
+            return "redirect:/transaction/create?accountId=" + accountId + "&balance=" + balance;
+        }
         Transaction transaction = new Transaction();
         transaction.setAccount(accountRepository.findById(accountId).orElse(null));
         transaction.setType(transactionType);
