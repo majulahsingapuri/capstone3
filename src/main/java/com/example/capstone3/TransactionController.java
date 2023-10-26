8e6a2a0559c2424dea99a77a1833f1fdade382bf
cori-hwf <100427925+cori-hwf@users.noreply.github.com>
2023-10-26 11:06:34 +0800

    Check sufficient balance for new debit (#12)
    * Check sufficient balance for new debit
    
    * Add missing accountRepository
    
    * duplicate function
    
    * Add error alert
    
    ---------
    
    Co-authored-by: Bhargav Singapuri <bhsingapuri@gmail.com>

--- src/main/java/com/example/capstone3/TransactionController.java
+++ src/main/java/com/example/capstone3/TransactionController.java
@@ -8,26 +8,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/transaction")
 public class TransactionController {
 
   @Autowired private TransactionRepository transactionRepository;
-
-  @Autowired AccountRepository accountRepository;
-
-  @RequestMapping("/create")
-  public String showCreateTransaction(@RequestParam("accountId") Long accountId, Model model) {
-    model.addAttribute("accountId", accountId);
-    return "createtransaction";
-  }
+  @Autowired private AccountRepository accountRepository;
 
   @PostMapping("/create")
   public String createTransaction(
       @RequestParam("accountId") Long accountId,
       @RequestParam("transactionType") TransactionType transactionType,
-      @RequestParam("amount") double amount) {
+      @RequestParam("amount") double amount,
+      RedirectAttributes redirectAttributes) {
+    Double balance = transactionRepository.getBalance(accountId);
+    if (transactionType.equals(TransactionType.Debit) && amount > balance) {
+      redirectAttributes.addFlashAttribute(
+          "error", "Insufficient balance. Please enter a smaller amount.");
+
+      return "redirect:/transaction/create?accountId=" + accountId;
+    }
     Transaction transaction = new Transaction();
     transaction.setAccount(accountRepository.findById(accountId).orElse(null));
     transaction.setType(transactionType);
@@ -35,5 +37,11 @@ public class TransactionController {
     transaction.setDatecreated(LocalDate.now());
     transactionRepository.save(transaction);
     return "redirect:/account/view/" + accountId;
+  }
+
+  @RequestMapping("/create")
+  public String showCreateTransaction(@RequestParam("accountId") Long accountId, Model model) {
+    model.addAttribute("accountId", accountId);
+    return "createtransaction";
   }
 }
